39bdb2ae37cc5135bced3bcedf7419423b0966eb
s1rfs3dat <60926347+s1rfs3dat@users.noreply.github.com>
2022-07-09 19:54:40 +0300

    Update CrudRepository.java

--- week-2/src/com/proje/repository/CrudRepository.java
+++ week-2/src/com/proje/repository/CrudRepository.java
@@ -1,9 +1,7 @@
 package com.proje.repository;
 import java.util.List;
 public interface CrudRepository<Type> {
-//// CRUD --> Create, Read, Update, Delete
-
-    List<Type> findAll();//bulmak için
+    List<Type> findAll();
     Type findById(int id);
     void saveToDatabase(Type object);
     void deleteFromDatabase(Type object);
