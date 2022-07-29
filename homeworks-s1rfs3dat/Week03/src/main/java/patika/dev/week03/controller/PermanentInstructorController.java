ab338f266acca7645d82d0672df8e1a6cefbf4c2
s1rfs3dat <60926347+s1rfs3dat@users.noreply.github.com>
2022-07-29 14:10:06 +0300

    Add files via upload

---
+++ homeworks-s1rfs3dat/Week03/src/main/java/patika/dev/week03/controller/PermanentInstructorController.java
@@ -0,0 +1,43 @@
+package patika.dev.week03.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import patika.dev.week03.models.PermanentInstructor;
+import patika.dev.week03.service.Interface.IInstructorService;
+
+import java.util.List;
+
+@RestController
+public class PermanentInstructorController {
+
+    IInstructorService iInstructorService;
+
+    @Autowired
+    public PermanentInstructorController(IInstructorService iInstructorService) {
+        this.iInstructorService = iInstructorService;
+    }
+
+    @GetMapping("/controller")
+    public ResponseEntity<List<PermanentInstructor>> getAllCourse() {
+        return new ResponseEntity<>(iInstructorService.findAll(), HttpStatus.OK);
+
+
+    }
+
+    @PostMapping("/controller")
+    public ResponseEntity<PermanentInstructor> saveCourse(@RequestBody PermanentInstructor permanentInstructor) {
+
+        return new ResponseEntity<>((PermanentInstructor) iInstructorService.save(permanentInstructor), HttpStatus.OK);
+
+
+    }
+
+
+
+
+}
---
+++ homeworks-s1rfs3dat/Week03/src/main/java/patika/dev/week03/dao/Interface/ICourseDAO.java
@@ -0,0 +1,12 @@
+package patika.dev.week03.dao.Interface;
+
+
+import org.springframework.stereotype.Repository;
+import patika.dev.week03.dao.Interface.IBaseDAO;
+
+@Repository
+public interface ICourseDAO<Course> extends IBaseDAO<Course> {
+
+
+
+}
